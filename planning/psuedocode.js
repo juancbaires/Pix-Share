541e28368016ef7d8ffc19377e32c5414bc65e03
Juan Baires <juancbaires@gmail.com>
2018-10-01 10:24:02 -0400

    planning folder created

---
+++ planning/psuedocode.js
@@ -0,0 +1,10 @@
+// start by setting up all my dependecies and using express as a server
+//create my user model that will have email, password
+//create my comment model that will have a type string, max length and like options
+//create my image model (this will require some research). Add the comment to it
+// create my main views with a nav bar that will stay the same across the app
+//create my sign up view simple email/password form
+//create an index view that will show all photos posted
+// create a photo show where user can comment on it
+// create a user show view where user can see their or some elses enitre album
+//all view will require some type of routing and controlling to handle functionality of this app
