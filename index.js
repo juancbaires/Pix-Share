19eb61b4733d4e12338e79b865baf1b106d49d0e
Juan Baires <juancbaires@gmail.com>
2018-10-04 13:56:17 -0400

    heroku

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 //list of dependencies
-const express = require("express");
+const express = require('express');
 const app = express();
-const flash = require("connect-flash");
-const hbs = require("hbs");
-const morgan = require("morgan");
-const cookieParser = require("cookie-parser");
-const parser = require("body-parser");
-const session = require("express-session");
-const passport = require("passport");
-const routes = require('./routes/user')
-const methodOverride = require('method-override')
-const multer = require('multer')
+const flash = require('connect-flash');
+const hbs = require('hbs');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const parser = require('body-parser');
+const session = require('express-session');
+const passport = require('passport');
+const routes = require('./routes/user');
+const methodOverride = require('method-override');
+const multer = require('multer');
 //multer, multer-gridfs-storage, gridfs-stream, method-override to upload?
 // See if I can get file uploads to work!
 
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(parser());
-app.use(express.static(__dirname + '/public/photo-storage')); 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public/photo-storage'));
+app.use(express.static(__dirname + '/public'));
 
-app.use(parser.urlencoded({extended: true}))
+app.use(parser.urlencoded({ extended: true }));
 app.set('view engine', 'hbs');
 
-app.use(session({ secret: "PIX-SHARE" }));
+app.use(session({ secret: 'PIX-SHARE' }));
 app.use(flash());
 
-require("./config/passport")(passport);
+require('./config/passport')(passport);
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -37,8 +37,6 @@ app.use(function(req, res, next) {
 
 app.use(routes);
 
-
-
-app.set('port', process.env.PORT || 3000) 
+app.set('port', process.env.PORT || 3000);
 
 app.listen(app.get('port'), () => console.log('server is running'));
